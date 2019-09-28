699c58bc9679c4f559d24a5b063c87e02aed3bc4
Germantru98 <germantruhanov@gmail.com>
2019-09-28 19:27:41 +0400

    Добавьте файлы проекта.

---
+++ DataPharser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption_Task9
+{
+    public class DataPharser
+    {
+        public string getMessage()
+        {
+            using (StreamReader stream = new StreamReader("Message.txt", Encoding.Default))
+            {
+                return stream.ReadToEnd().ToUpper();
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption_Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataPharser dp = new DataPharser();
+            Encryptor encryptor = new Encryptor();
+            var message = dp.getMessage();
+            Console.Write("Введите пароль: ");
+            var pass = Console.ReadLine();
+            var encryptedMessageByPass = encryptor.Encrypt(message, pass);
+            Console.WriteLine("Зашифрованное сообщение {0}", encryptedMessageByPass);
+            Console.WriteLine("Расшифрованное сообщение {0}", encryptor.Decrypt(encryptedMessageByPass, pass));
+        }
+    }
+}
